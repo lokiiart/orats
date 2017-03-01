c43aa292dc9716f8be7bf27f68bc53b3610b603c
lokiiart <lokiiart@gmail.com>
2017-03-01 20:22:37 +0800

    Wed Mar  1 20:22:37 CST 2017

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,7 +57,7 @@ task :deploy do
   invoke :'git:ensure_pushed'
   deploy do
     invoke :'git:clone'
-    command %{docker-compose stop}
+    # command %{docker-compose stop}
     command %{docker-compose up --build -d}
     # command 'docker-compose exec --user "$(id -u):$(id -g)" website rails assets:precompile'
     # command 'docker-compose exec --user "$(id -u):$(id -g)" website rails db:migrate'
