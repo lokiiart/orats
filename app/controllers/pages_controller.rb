1048fbb2cd612cf862e7a6c924f03824fe0304d7
lokiiart <lokiiart@gmail.com>
2017-03-01 10:04:45 +0800

    Wed Mar  1 10:04:45 CST 2017

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -30,6 +30,7 @@ class PagesController < ApplicationController
       # @page = 'pages/home'
     else
       # @page = 'pages/baidu_list'
+      # @page = 'pages/baidu_flow_enter'
       @page = 'pages/home'
     end
 
